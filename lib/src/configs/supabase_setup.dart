31f4226a9062a69732641d53f634ac8a1b945921
Tanzeel ur Rehman <reman.tanzeel481@gmail.com>
2024-07-17 10:53:43 +0500

    Update supabase_setup.dart

--- lib/src/configs/supabase_setup.dart
+++ lib/src/configs/supabase_setup.dart
@@ -3,7 +3,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 class SupabaseSetup {
   static const String _supabaseUrl = 'https://evimsreigandkjuinogy.supabase.co';
   static const String _supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2aW1zcmVpZ2FuZGtqdWlub2d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcxMDgxMTksImV4cCI6MjAxMjY4NDExOX0.I1o3VtJ8nBUAUHvee-mtDd3AoNxzrMxDkZ9Vm4l9-uA';
+      'YOUR-SUPABASE-KEY';
 
   static Future<void> init() async {
     await Supabase.initialize(
